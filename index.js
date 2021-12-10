969f9bb35cc76f86bc637372112d47ec90bbfd6d
555ekstase <martinbibow@gmx.de>
2021-12-10 14:35:26 +0100

    1st commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(express.json());
+
+app
+  .route("/")
+  .get((req, res) =>
+    res.send(
+      "<h2>Mögliche Endpunkte:</h2><p>/title : um alle Rezepttitel zu sehen</p><p>/home_picture : um die Rezeptbilder zu sehen</p>"
+    )
+  );
+
+app.get('/recipes'), (req, res)  => console.log("SELECT * FROM recipes");
+app.get("/recipes/:id", (req, res) => console.log("SELECT * FROM recipes WHERE id=id"));
+
+app.get('/authors'), (req, res)  => console.log("SELECT * FROM authors");
+app.get("/authors/:id", (req, res) => console.log("SELECT * FROM authors WHERE id=id"));
+
+app.listen(port, () => console.log(`Server is listening at http://localhost:${port}`));
