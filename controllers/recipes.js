6828bdddd1265ebfad51e34e18d70ee365801a98
555ekstase <martinbibow@gmx.de>
2021-12-12 09:16:17 +0100

    added env to gitignore and adedd recipes.js

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,8 +1,15 @@
 import pool from '../db/pg.js';
 export const getAllRecipes = (req, res) => {
-//SELECT * FROM recipes
+ pool
+    .query("SELECT * from recipes")
+    .then((data) => res.json({ users: data.rows }))
+    .catch((err) => console.log(err));
 }
 
  export const getSingleRecipe = (req, res) => {
-//SELECT * FROM recipes WHERE id=id
+ const id = req.params.id;
+  pool
+    .query(`SELECT * from recipes WHERE id=${id}`)
+    .then((data) => res.json(data.rows[0]))
+    .catch((err) => console.log(err));
 }
