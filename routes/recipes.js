64969572a4056873efb1612b2e8ba2e8b239ca82
555ekstase <martinbibow@gmx.de>
2021-12-15 18:13:42 +0100

    hfujihwekujf

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,9 +1,13 @@
-import express from 'express';
-import { createRecipe, getAllRecipes, getSingleRecipe } from '../controllers/recipes.js';
+import express from "express";
+import {
+  createRecipe,
+  getAllRecipes,
+  getSingleRecipe,
+  deleteSingleRecipe
+} from "../controllers/recipes.js";
 const recipes = express.Router();
 
 recipes.route("/").get(getAllRecipes).post(createRecipe);
 recipes.route("/:id").get(getSingleRecipe).delete(deleteSingleRecipe);
-
 
 export default recipes;
