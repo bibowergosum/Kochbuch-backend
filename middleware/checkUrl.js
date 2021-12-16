b1bf9ed5b05c9b5b870acfe6167fa7ce146ca8fa
JoniJo06 <jonasjuergens6@gmail.com>
2021-12-16 18:06:39 +0100

    fix leak

--- middleware/checkUrl.js
+++ middleware/checkUrl.js
@@ -4,7 +4,7 @@ export const checkUrl = (req, res, next) => {
 	const url = /* req.protocol + '://' +  */ req.get('origin') + req.originalUrl;
 	console.log(url);
 	console.log(req);
-	if (url.includes('local')) {
+	if (url.includes('https://keen-turing-04ea0a.netlify.app/')) {
 		next();
 	} else {
 		res.status(403).send('du kommst hier ned rein');
