1bd8a2a6efb195e3469c0e2169c5d5ef4f9c285d
JoniJo06 <jonasjuergens6@gmail.com>
2021-12-16 11:22:37 +0100

    fix delete function

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,54 +1,36 @@
-import pool from "../db/pg.js";
+/** @format */
+
+import pool from '../db/pg.js';
 export const getAllRecipes = (req, res) => {
-  pool
-    .query("SELECT * FROM recipes ORDER BY id")
-    .then((data) => res.json({ recipes: data.rows }))
-    .catch((err) => console.log(err));
+	pool
+		.query('SELECT * FROM recipes ORDER BY id')
+		.then((data) => res.json({ recipes: data.rows }))
+		.catch((err) => console.log(err));
 };
 
 export const getSingleRecipe = (req, res) => {
-  const id = req.params.id;
-  pool
-    .query(`SELECT * FROM recipes WHERE id=$1`, [id])
-    .then((data) => res.json(data.rows[0]))
-    .catch((err) => console.log(err));
+	const id = req.params.id;
+	pool
+		.query(`SELECT * FROM recipes WHERE id=$1`, [id])
+		.then((data) => res.json(data.rows[0]))
+		.catch((err) => console.log(err));
 };
 
 export const createRecipe = (req, res) => {
-  const {
-    title,
-    ingredients,
-    instructions,
-    home_picture,
-    home_picture_alt,
-    picture_one,
-    picture_one_alt,
-    picture_two,
-    picture_two_alt,
-  } = req.body;
-  pool
-    .query(
-      "INSERT INTO recipes (title, ingredients, instructions, home_picture, home_picture_alt, picture_one, picture_one_alt, picture_two, picture_two_alt) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);",
-      [
-        title,
-        ingredients,
-        instructions,
-        home_picture,
-        home_picture_alt,
-        picture_one,
-        picture_one_alt,
-        picture_two,
-        picture_two_alt,
-      ]
-    )
-    .then((data) => res.json("hihihi"))
-    .catch((err) => console.log(err));
+	const { title, ingredients, instructions, home_picture, home_picture_alt, picture_one, picture_one_alt, picture_two, picture_two_alt } = req.body;
+	pool
+		.query(
+			'INSERT INTO recipes (title, ingredients, instructions, home_picture, home_picture_alt, picture_one, picture_one_alt, picture_two, picture_two_alt) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);',
+			[title, ingredients, instructions, home_picture, home_picture_alt, picture_one, picture_one_alt, picture_two, picture_two_alt]
+		)
+		.then((data) => res.json('hihihi'))
+		.catch((err) => console.log(err));
 };
 
-export const deleteSingleRecipe = () => {
-  const { id } = req.params.id;
-
-  pool.query('DELETE FROM recipes WHERE id=$1', [id])
-    .then(data => res.send('erfolgreich gelöscht'))
-  .catch(err => res.json(err))
-}
+export const deleteSingleRecipe = (req, res) => {
+	const id = req.params.id;
+	pool
+		.query('DELETE FROM recipes WHERE id=$1', [id])
+		.then(res.send('erfolgreich gelöscht'))
+		.catch((err) => res.json(err));
+};
